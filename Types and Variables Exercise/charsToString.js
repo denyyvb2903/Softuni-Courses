f12c758893985dfc425687227f0273ea4b2661ad
denyyvb2903 <denislav.chakarov@gmail.com>
2023-01-26 15:38:57 +0200

    ola

---
+++ Types and Variables Exercise/charsToString.js
@@ -0,0 +1,13 @@
+function solve(char1, char2, char3) {
+
+    char1 = char1.toString();
+    char2 = char2.toString();
+    char3 = char3.toString();
+
+    console.log(`${char1}${char2}${char3}`);
+
+}
+solve('%',
+'2',
+'o'
+);
